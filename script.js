27983ed161d61c6fe68a741b8d82d1781bc95eeb
uswa.r <uswa.r@outlook.com>
2022-09-11 00:30:01 +0500

    .

--- script.js
+++ script.js
@@ -2,18 +2,18 @@
 
 
 const months = [
-    'January',
-    'February',
+    'Jan',
+    'Feb',
     'March',
     'April',
     'May',
     'June',
     'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
 ];
 const weekdays = [
     'Sunday',
@@ -28,4 +28,19 @@ const weekdays = [
 
 const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
-const giveaway = document.querySelector('.giveaway');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let futureDate = new Date(2022, 11, 15, 10, 30, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minuts = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+
+const date = futureDate.getDate();
+
+const weekday = weekdays[futureDate.getDay()];
+
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minuts}AM`;
