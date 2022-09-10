b3f8043dae7a94bf910480658050067722b5c515
uswa.r <uswa.r@outlook.com>
2022-09-10 23:48:04 +0500

    .

--- script.js
+++ script.js
@@ -29,3 +29,12 @@ const weekdays = [
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
 const minutes = futureDate.getMinutes();
+
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
+// months are ZERO index based;
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
+
+// let futureDate = new Date(2020, 3, 24, 11, 30, 0);
