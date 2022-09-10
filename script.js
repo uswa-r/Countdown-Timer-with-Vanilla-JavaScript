33a964ea9a11a6a0d76ace12818ae866737b9404
uswa.r <uswa.r@outlook.com>
2022-09-10 23:46:33 +0500

    db

---
+++ script.js
@@ -0,0 +1,27 @@
+//static database
+
+
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+];
+const weekdays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+];
+//database ends here
