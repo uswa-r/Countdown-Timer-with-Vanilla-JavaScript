6f80ca7ce38a1814d5fec983e5ca19258afb99e6
uswa.r <uswa.r@outlook.com>
2022-09-10 23:49:06 +0500

    .

--- script.js
+++ script.js
@@ -70,3 +70,24 @@ function getRemaindingTime() {
     let hours = Math.floor((t % oneDay) / oneHour);
     let minutes = Math.floor((t % oneHour) / oneMinute);
     let seconds = Math.floor((t % oneMinute) / 1000);
+
+
+    // set values array
+    const values = [days, hours, minutes, seconds];
+
+    function format(item) {
+        if (item < 10) {
+            return (item = `0${item}`);
+        }
+        return item;
+    }
+
+    items.forEach(function(item, index) {
+        item.innerHTML = format(values[index]);
+    });
+
+    if (t < 0) {
+        clearInterval(countdown);
+        deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired!</h4>`;
+    }
+}
